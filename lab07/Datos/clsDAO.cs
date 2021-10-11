e932f56764f4e70a2e47cb3bd3551e62d8de8e5e
unknown <Daniel Bedregal>
2021-10-11 12:39:54 -0500

    Lab 07 procedimiento

---
+++ lab07/Datos/clsDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace Datos
+{
+    public class clsDAO
+    {
+        public SqlConnection conn;
+
+        public clsDAO() {
+            string str = "Server=DESKTOP-4UPQ8HT\\SQLEXPRESS01;Database=School;Integrated Security=true";
+            conn = new SqlConnection(str);
+        }
+    }
+}
