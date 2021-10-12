73337963924c8db6255ee2d1c8efa5d9dc7e8704
unknown <Daniel Bedregal>
2021-10-11 20:34:32 -0500

    Lab06 completo

---
+++ lab06/ManPerson.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace lab06
+{
+    partial class ManPerson
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dtpEnrollment = new System.Windows.Forms.DateTimePicker();
+            this.dtpHire = new System.Windows.Forms.DateTimePicker();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPersonID = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.dgvListado = new System.Windows.Forms.DataGridView();
+            this.btnOrderByLastName = new System.Windows.Forms.Button();
+            this.btnSearchByPersonID = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dtpEnrollment);
+            this.groupBox1.Controls.Add(this.dtpHire);
+            this.groupBox1.Controls.Add(this.txtApellido);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.txtPersonID);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(337, 182);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Persona";
+            // 
+            // dtpEnrollment
+            // 
+            this.dtpEnrollment.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpEnrollment.Location = new System.Drawing.Point(177, 150);
+            this.dtpEnrollment.Name = "dtpEnrollment";
+            this.dtpEnrollment.Size = new System.Drawing.Size(100, 20);
+            this.dtpEnrollment.TabIndex = 12;
+            // 
+            // dtpHire
+            // 
+            this.dtpHire.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpHire.Location = new System.Drawing.Point(23, 149);
+            this.dtpHire.Name = "dtpHire";
+            this.dtpHire.Size = new System.Drawing.Size(100, 20);
+            this.dtpHire.TabIndex = 11;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(177, 99);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 10;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(23, 99);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 7;
+            // 
+            // txtPersonID
+            // 
+            this.txtPersonID.Location = new System.Drawing.Point(23, 47);
+            this.txtPersonID.Name = "txtPersonID";
+            this.txtPersonID.Size = new System.Drawing.Size(100, 20);
+            this.txtPersonID.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(174, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Inscripción";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 133);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Contrato";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Apellido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Nombre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código";
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(386, 43);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 1;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(386, 72);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(386, 130);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 4;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(386, 101);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertar.TabIndex = 3;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(386, 159);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 5;
+            this.btnEliminar.Text = "Elimiinar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // dgvListado
+            // 
+            this.dgvListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListado.Location = new System.Drawing.Point(12, 200);
+            this.dgvListado.Name = "dgvListado";
+            this.dgvListado.ReadOnly = true;
+            this.dgvListado.RowHeadersVisible = false;
+            this.dgvListado.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvListado.Size = new System.Drawing.Size(449, 213);
+            this.dgvListado.TabIndex = 6;
+            this.dgvListado.SelectionChanged += new System.EventHandler(this.dgvListado_SelectionChanged);
+            // 
+            // btnOrderByLastName
+            // 
+            this.btnOrderByLastName.Location = new System.Drawing.Point(189, 419);
+            this.btnOrderByLastName.Name = "btnOrderByLastName";
+            this.btnOrderByLastName.Size = new System.Drawing.Size(121, 24);
+            this.btnOrderByLastName.TabIndex = 7;
+            this.btnOrderByLastName.Text = "Ordenar por Apellido";
+            this.btnOrderByLastName.UseVisualStyleBackColor = true;
+            this.btnOrderByLastName.Click += new System.EventHandler(this.btnOrderByLastName_Click);
+            // 
+            // btnSearchByPersonID
+            // 
+            this.btnSearchByPersonID.Location = new System.Drawing.Point(316, 419);
+            this.btnSearchByPersonID.Name = "btnSearchByPersonID";
+            this.btnSearchByPersonID.Size = new System.Drawing.Size(121, 24);
+            this.btnSearchByPersonID.TabIndex = 8;
+            this.btnSearchByPersonID.Text = "Buscar por Código";
+            this.btnSearchByPersonID.UseVisualStyleBackColor = true;
+            this.btnSearchByPersonID.Click += new System.EventHandler(this.btnOrderByPersonID_Click);
+            // 
+            // ManPerson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 454);
+            this.Controls.Add(this.btnSearchByPersonID);
+            this.Controls.Add(this.btnOrderByLastName);
+            this.Controls.Add(this.dgvListado);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ManPerson";
+            this.Text = "Mantenimiento de Personas";
+            this.Load += new System.EventHandler(this.ManPerson_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dtpEnrollment;
+        private System.Windows.Forms.DateTimePicker dtpHire;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPersonID;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.DataGridView dgvListado;
+        private System.Windows.Forms.Button btnOrderByLastName;
+        private System.Windows.Forms.Button btnSearchByPersonID;
+    }
+}
+
---
+++ lab06/ManPerson.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace lab06
+{
+    public partial class ManPerson : Form
+    {
+        SqlConnection con;
+        DataSet ds = new DataSet();
+        DataTable tablePerson = new DataTable();
+        public ManPerson()
+        {
+            InitializeComponent();
+        }
+
+        private void ManPerson_Load(object sender, EventArgs e)
+        {
+            String str = "Server=DESKTOP-4UPQ8HT\\SQLEXPRESS01;Database=School;Integrated Security=true;";
+            con = new SqlConnection(str);
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            String sql = "SELECT * FROM Person";
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = cmd;
+
+            //llenamos el dataset con una tabla llamada Person
+            adapter.Fill(ds, "Person");
+
+            //Asignamos esa tabla a un dataset a un objeto Table
+            //Para trabajar directamente en él
+            tablePerson = ds.Tables["Person"];
+
+            dgvListado.DataSource = tablePerson;
+            dgvListado.Update();
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("InsertPerson", con);
+
+            cmd.Parameters.Add("@LastName", SqlDbType.VarChar,50, "LastName");
+            cmd.Parameters.Add("@FirstName", SqlDbType.VarChar, 50, "FirstName");
+            cmd.Parameters.Add("@HireDate", SqlDbType.Date).SourceColumn="HireDate";
+            cmd.Parameters.Add("@EnrollmentDate", SqlDbType.Date).SourceColumn= "EnrollmentDate";
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.InsertCommand = cmd;
+            adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
+
+            //creamos una fila nueva la cual insertaremos en la base de datos
+            //Esta fila debe tener la estructura correspondiente
+            DataRow fila = tablePerson.NewRow();
+            fila["LastName"] = txtApellido.Text;
+            fila["FirstName"] = txtNombre.Text;
+            fila["HireDate"] = dtpHire.Value;
+            fila["EnrollmentDate"] = dtpEnrollment.Value;
+
+            //U(na vez tenemos la fila la agregamos a la tabla Person del Dataset
+            tablePerson.Rows.Add(fila);
+
+            //Actualizamos el dataset con la tabla person
+            adapter.Update(tablePerson);
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("UpdatePerson", con);
+
+            cmd.Parameters.Add("@PersonID", SqlDbType.VarChar).SourceColumn = "PersonID";
+            cmd.Parameters.Add("@LastName", SqlDbType.VarChar).SourceColumn = "LastName";
+            cmd.Parameters.Add("@FirstName", SqlDbType.VarChar).SourceColumn = "FirstName";
+            cmd.Parameters.Add("@HireDate", SqlDbType.Date).SourceColumn = "HireDate";
+            cmd.Parameters.Add("@EnrollmentDate", SqlDbType.Date).SourceColumn = "EnrollmentDate";
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.InsertCommand = cmd;
+            adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
+
+            //creamos un array de DataRow el cual almacenara la fila que coincida con el resultado de la busqueda de persona ID
+            //A cada campo de la fila, le asignamos el valor modificado.
+            DataRow[] fila = tablePerson.Select("PersonID = '"+ txtPersonID.Text+"'");
+            fila[0]["LastName"] = txtApellido.Text;
+            fila[0]["FirstName"] = txtNombre.Text;
+            fila[0]["HireDate"] = dtpHire.Value;
+            fila[0]["EnrollmentDate"] = dtpEnrollment.Value;
+
+            //Actualizamos el dataset con la tabla person
+            adapter.Update(tablePerson);
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("DeletePerson", con);
+            cmd.Parameters.Add("@PersonID", SqlDbType.VarChar).SourceColumn = "PersonID";
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.InsertCommand = cmd;
+            adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
+
+            //Buscamos la fila a eliminar
+            DataRow[] fila = tablePerson.Select("PersonID = '" + txtPersonID.Text + "'");
+
+            //Eliminamos  de la tabla de la fila especificada
+            tablePerson.Rows.Remove(fila[0]);
+
+            // actualizamos el dataset con la tabal modificada
+            adapter.Update(tablePerson);
+        }
+
+        private void btnOrderByLastName_Click(object sender, EventArgs e)
+        {
+            DataView dv = new DataView(tablePerson);
+            dv.Sort = "LastName ASC"; 
+            dgvListado.DataSource = dv;
+        }
+
+        private void btnOrderByPersonID_Click(object sender, EventArgs e)
+        {
+            DataView dv = new DataView(tablePerson);
+            dv.RowFilter = "PersonID = '"+ txtPersonID.Text+"'";
+            dgvListado.DataSource = dv;
+        }
+
+        private void dgvListado_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvListado.SelectedRows.Count > 0) {
+                txtPersonID.Text = dgvListado.SelectedRows[0].Cells[0].Value.ToString();
+                txtNombre.Text = dgvListado.SelectedRows[0].Cells[1].Value.ToString();
+                txtApellido.Text = dgvListado.SelectedRows[0].Cells[2].Value.ToString();
+
+                String hireDate = dgvListado.SelectedRows[0].Cells[3].Value.ToString();
+                if (String.IsNullOrEmpty(hireDate))
+                    dtpHire.Checked = false;
+                else
+                    dtpHire.Text = hireDate;
+                String enrollmentDate = dgvListado.SelectedRows[0].Cells[3].Value.ToString();
+                if (String.IsNullOrEmpty(hireDate))
+                    dtpEnrollment.Checked = false;
+                else
+                    dtpEnrollment.Text = enrollmentDate;
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            DataView dv = new DataView(tablePerson);
+            dv.RowFilter = "PersonID = '" + txtPersonID.Text + "' OR FirstName = '" + txtNombre.Text + "' OR LastName = '" + txtApellido.Text + "'";
+            dgvListado.DataSource = dv;
+        }
+    }
+}
