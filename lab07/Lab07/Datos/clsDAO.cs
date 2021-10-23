16d07ef2c35a073d1e22ea87cbb8444046d0ae54
unknown <Daniel Bedregal>
2021-10-23 00:01:47 -0500

    Agregar capa presentacion web a lab07

--- lab07/Lab07/Datos/clsDAO.cs
+++ lab07/Lab07/Datos/clsDAO.cs
@@ -4,15 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+
 namespace Datos
 {
     public class clsDAO
     {
-        public SqlConnection conn;
+        public SqlConnection con;
 
         public clsDAO() {
-            string str = "Server=DESKTOP-4UPQ8HT\\SQLEXPRESS01;Database=School;Integrated Security=true";
-            conn = new SqlConnection(str);
+            String str = "Server=DESKTOP-LMO2I3Q\\LOCAL;Database=School;Integrated Security=true";
+            con = new SqlConnection(str);
         }
+        
     }
 }
--- lab07/Lab07/Negocio/clsNegPerson.cs
+++ lab07/Lab07/Negocio/clsNegPerson.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data;
 using Datos;
+using System.Data;
+
 namespace Negocio
 {
     public class clsNegPerson
     {
         clsDAOPerson daoPerson = new clsDAOPerson();
+
         public DataTable GetAll() {
             return daoPerson.GetAll();
+        }
+
+        public DataTable GetSearch(String busqueda)
+        {
+            return daoPerson.GetSearch(busqueda);
         }
     }
 }
