f984623e46cbaeccecdd4adfd1dd8b3a1ec8986b
unknown <Daniel Bedregal>
2021-11-03 23:49:05 -0500

    Laboratorio 11 completo

---
+++ SchoolApp/SchoolBLL/CPersonBLL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolDAL;
+using SchoolEntities;
+
+namespace SchoolBLL
+{
+    public class CPersonBLL
+    {
+        CPersonDAL personDal = new CPersonDAL();
+
+        public List<CPerson> ListarBLL() {
+            return personDal.listar();
+        }
+    }
+}
---
+++ SchoolApp/SchoolEntities/CPerson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolEntities
+{
+    public class CPerson
+    {
+        public int PersonId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime HireDate { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
