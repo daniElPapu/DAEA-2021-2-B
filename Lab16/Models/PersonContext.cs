72b6524194dc1669be2b40d27247501363aff601
unknown <Daniel Bedregal>
2021-12-07 23:27:16 -0500

    Laboratorio 16 culminado

---
+++ Lab16/Models/PersonContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+
+namespace persons.Models
+{
+    public class PersonContext: DbContext
+    {
+        public PersonContext(DbContextOptions<PersonContext> options):base(options)
+        {
+        
+        }
+
+        public DbSet<Person> Person {get; set;}
+    }
+}
